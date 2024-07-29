7bbec9dd5861d3fcf29e317a6780803115ce3c92
Sunken-Wood <SunkenWood@outlook.com>
2024-07-29 11:47:31 +0800

    upload

---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+import mindustry.*;
+import arc.Core;
+import arc.Events;
+import arc.util.Log;
+import arc.util.Time;
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class Main extends Mod {
+    public Main(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+    @Override
+    public void loadContent() {
+        super.loadContent();
+    }
+}
