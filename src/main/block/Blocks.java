cdd95201f320f596bd2302e11f8963764b5b66a7
Sunken-Wood <SunkenWood@outlook.com>
2024-07-29 12:40:32 +0800

    upload

---
+++ src/main/block/Blocks.java
@@ -0,0 +1,13 @@
+package main.block;
+
+import main.ContentList;
+import mindustry.world.blocks.campaign.LaunchPad;
+
+public class Blocks implements ContentList {
+    public static LaunchPad launchPad;
+
+    @Override
+    public void load() {
+        launchPad = new LaunchPad("launch-pad");
+    }
+}
