1c9b3bdd46435b596c4e18a01d5b39e4c3467dbb
Sunken-Wood <SunkenWood@outlook.com>
2024-07-30 23:02:18 +0800

    upload

--- src/main/content/Planets.java
+++ src/main/content/Planets.java
@@ -26,8 +26,8 @@ public class Planets implements ContentList {
             generator = new ErekirPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 5); // division 划分
             cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 2, 2.5f, 1.5f, 5, Color.rgb(150,150,255).a(0.72f), 2, 0.42f, 1f, 0.43f),
-                    new HexSkyMesh(this, 3, 2.8f, 1.7f, 5, Color.rgb(100, 100, 255).a(0.72f), 2, 0.42f, 1f, 0.44f)
+                    new HexSkyMesh(this, 2, 0.5f, 0.15f, 5, Color.rgb(150,150,255).a(0.72f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.8f, 0.17f, 5, Color.rgb(100, 100, 255).a(0.72f), 2, 0.42f, 1f, 0.44f)
             );
             landCloudColor = Color.rgb(120, 120, 255);
             atmosphereColor =Color.rgb(255, 150, 150);
