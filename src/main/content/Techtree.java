aa440f0f82c443d2f41cf32a62dff550e7bfa220
Sunken-Wood <SunkenWood@outlook.com>
2024-07-31 13:16:55 +0800

    upload

--- src/main/content/Techtree.java
+++ src/main/content/Techtree.java
@@ -5,18 +5,19 @@ import mindustry.content.TechTree;
 
 import static main.content.Blocks.launchPad_erekir;
 import static main.content.Planets.destruction;
+import static mindustry.content.Planets.erekir;
 import static mindustry.content.TechTree.node;
 import static mindustry.content.TechTree.nodeRoot;
+import static mindustry.content.ErekirTechTree.*;
 
 public class Techtree implements ContentList {
     public static TechTree.TechNode wooden_tools;
 
     @Override
     public void load() {
+        erekir.techTree.children.add(node(launchPad_erekir));
         wooden_tools = nodeRoot("wooden-tools", destruction, () -> {
-            node(launchPad_erekir, () -> {
 
-            });
         });
     }
 }
