82ef1e668052d851411127d339ade01612c63073
Sunken-Wood <SunkenWood@outlook.com>
2024-07-31 12:47:40 +0800

    upload

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,15 +7,19 @@ import arc.util.Time;
 import main.content.Blocks;
 import main.content.Planets;
 import main.content.Techtree;
+import main.ui.WUI;
 import mindustry.game.EventType;
 import mindustry.mod.Mod;
 import mindustry.ui.dialogs.BaseDialog;
+
+import static mindustry.Vars.ui;
 
 public class Main extends Mod {
 
     public static Blocks blocks;
     public static Planets planets;
     public static Techtree techtree;
+    public static WUI wui;
 
 
     public Main(){
@@ -30,5 +34,9 @@ public class Main extends Mod {
         blocks.load();
         planets.load();
         techtree.load();
+        if (ui != null){
+            wui = new WUI();
+            ui.menufrag.addButton("Hello", () -> {wui.load();});
+        }
     }
 }
