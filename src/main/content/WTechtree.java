6d842732d4e62098d0745f079be9ee025327a61e
Sunken-Wood <SunkenWood@outlook.com>
2024-08-25 11:43:45 +0800

    Update WTechtree.java

--- src/main/content/WTechtree.java
+++ src/main/content/WTechtree.java
@@ -16,29 +16,7 @@ public class WTechtree implements ContentList {
     public void load() {
         addToNode(Blocks.turbineCondenser, node(steam_turbine));
         addToNode(Blocks.payloadMassDriver,node(launchPad_erekir));
-
-
-        erekir.techTree.children.add(node(launchPad_erekir, () -> {
-            node(fortify_the_defensive_wall,() -> {
-                node(fortify_the_defensive_wall_large,() -> {
-                    node(strengthened_gates);
-                });
-                    });
-            node(sand_maker, () -> {
-                node(steam_turbine, () -> {
-                    node(reinforce_pump_plus, () -> {
-                        node(water_extractor_erekir);
-                        node(t2_vent_condenser);
-                    });
-                });
-                node(duct_bridge_plus, () -> {
-                   node(reinforced_bridge_conduit_plus);
-                });
-            });
-        }));
-
-
-
+        
 //        destruction
         destruction.techTree = nodeRoot(toText("planet.wooden-mindustry-destruction.name"), destruction_core, () -> {
             node(WItems.iron, () -> {
