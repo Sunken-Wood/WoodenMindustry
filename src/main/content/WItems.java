ca6b1c46fde8e93fa55edf7549e4333b2653f7d2
TAGAST <TAGAST@qq.com>
2024-11-16 17:23:44 +0800

    增加了毁灭星球的物品

--- src/main/content/WItems.java
+++ src/main/content/WItems.java
@@ -16,32 +16,32 @@ public class WItems implements ContentList {
     public void load() {
         // 铁
         iron = new Item("iron"){{
-            hardness = 1; // 开采等级
+            hardness = 3; // 开采等级
             color = Color.valueOf("7E7B7A");
         }};
         // 铜
         copper = new Item("copper"){{
-            hardness = 1;
+            hardness = 3;
             color = Color.valueOf("B87333");
         }};
         // 铝
         aluminum = new Item("aluminum"){{
-            hardness = 1;
+            hardness = 3;
             color = Color.valueOf("C0C0C0");
         }};
         //铅
         leab = new Item("leab"){{
-            hardness = 1;
+            hardness = 3;
             color = Color.valueOf("7A7B7D");
         }};
         // 金
         gold = new Item("gold"){{
-            hardness = 2;
+            hardness = 4;
             color = Color.valueOf("ffd700");
         }};
         // 银
         silver = new Item("silver"){{
-            hardness = 3;
+            hardness = 4;
             color = Color.valueOf("D3D3D3");
         }};
         destructionItems.addAll(
